66e2cec14cbe0dbcea56e030aef511294a7a04b3
philomuzzi <gxlee1986@gmail.com>
2014-10-17 13:59:45 +0800

    递归的解法，但是是参考了别人的解法

--- SymmetricTree/SymmetricTree.cpp
+++ SymmetricTree/SymmetricTree.cpp
@@ -8,27 +8,27 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-// 完全错误，题目就理解错误了
 class Solution {
 public:
     bool isSymmetric(TreeNode *root) {
-        if (root)
-        {
-            if (!root->left && !root->right)
-                return true;
-
-            if (root->left && root->right && root->left->val == root->right->val)
-            {
-                bool ret;
-                ret = isSymmetric(root->left);
-                ret = ret && isSymmetric(root->right);
-                return ret;
-            }
-
-            return false;
+        if (!root) {
+            return true;
         }
 
-        return true;
+        return isSymmetricHelper(root->left, root->right);
+    }
+
+    bool isSymmetricHelper(TreeNode *left, TreeNode *right)
+    {
+        if (!left && !right)
+            return true;
+        if (left && right && left->val == right->val)
+        {
+            if (isSymmetricHelper(left->left, right->right) && isSymmetricHelper(left->right, right->left))
+                return true;
+        }
+
+        return false;
     }
 };
 
