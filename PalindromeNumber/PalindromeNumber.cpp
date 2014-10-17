708b255d86621f52b68005b0b38500d64b8b86fd
philomuzzi <gxlee1986@gmail.com>
2014-10-17 17:44:59 +0800

    add Palindrome Number problem

---
+++ PalindromeNumber/PalindromeNumber.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        if (x % 10 == x)
+            return true;
+
+        int l = 1;
+        int t = x;
+
+        while (t / 10)
+        {
+            l *= 10;
+            t = t / 10;
+        }
+
+        while (x / 10)
+        {
+            if (x % 10 != x / l)
+                return false;
+
+            x %= l;
+            x /= 10;
+            l /= 100;
+        }
+
+        return true;
+    }
+
+    void Test(int x)
+    {
+        if (isPalindrome(x))
+            cout << x << " is Palindrome Number" << endl;
+        else
+            cout << x << " is not Palindrome Number" << endl;
+    }
+};
+
+int main()
+{
+    Solution s;
+    s.Test(12345);
+    s.Test(123321);
+    s.Test(-121);
+    s.Test(1);
+    s.Test(1000021);
+    return 0;
+}
