188215f63672321c639848d58656a72dc308ab03
philomuzzi <gxlee1986@gmail.com>
2014-10-20 10:09:03 +0800

    add SameTree problem

---
+++ SameTree/SameTree.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+//Definition for binary tree
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (!p && !q)
+            return true;
+
+        if (p && q)
+        {
+            if (p->val == q->val)
+            {
+                return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+//    Solution s;
+    return 0;
+}
