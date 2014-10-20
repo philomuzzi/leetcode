a63361b21c26d15c2691281a9e67dda6e4f03282
philomuzzi <gxlee1986@gmail.com>
2014-10-20 11:31:10 +0800

    add new method to solve it

--- MergeSortedArray/MergeSortedArray.cpp
+++ MergeSortedArray/MergeSortedArray.cpp
@@ -10,51 +10,57 @@ public:
         while (j < m && k < n && l < m+n)
         {
             if (A[j] <= B[k])
-            {
-                C[l] = A[j];
-                j++;
-            }
+                C[l++] = A[j++];
             else
-            {
-                C[l] = B[k];
-                k++;
-            }
-
-            l++;
+                C[l++] = B[k++];
         }
 
         if (j < m)
         {
             while (j < m && l < m+n)
-            {
-                C[l] = A[j];
-                l++;
-                j++;
-            }
+                C[l++] = A[j++];
         }
 
         if (k < n)
         {
             while (k < n && l < m+n)
-            {
-                C[l] = B[k];
-                l++;
-                k++;
-            }
+                C[l++] = B[k++];
         }
 
         for (int i = 0; i < m+n; ++i)
             A[i] = C[i];
     }
+
+    void merge2(int A[], int m, int B[], int n)
+    {
+        int i = m + n -1;
+        int j = m - 1;
+        int k = n - 1;
+
+        while (i >= 0)
+        {
+            if (j >= 0 && k >= 0)
+            {
+                if (A[j] >= B[k])
+                    A[i--] = A[j--];
+                else
+                    A[i--] = B[k--];
+            }
+            else if (k >= 0)
+                A[i--] = B[k--];
+            else
+                i = -1;
+        }
+    }
 };
 
 int main()
 {
-    int A[10] = {1};
-    int B[] = {};
+    int A[10] = {1, 7, 15, 23};
+    int B[] = {12, 18, 20};
 
     Solution s;
-    s.merge(A, 1, B, 0);
+    s.merge2(A, 4, B, 3);
 
     for (int i = 0; i < 10; ++i)
         cout << A[i] << endl;
